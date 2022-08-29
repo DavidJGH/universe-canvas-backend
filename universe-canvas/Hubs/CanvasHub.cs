884105cb25e1d686ea5a0a72558a2daa110da3b0
david herdlicka <david.herdlicka@gmail.com>
2022-08-29 11:55:07 +0200

    feat: update standard palette

--- universe-canvas/Hubs/CanvasHub.cs
+++ universe-canvas/Hubs/CanvasHub.cs
@@ -15,15 +15,15 @@ namespace universe_canvas.Hubs
             "#333c57",
             "#5d275d",
             "#b13e53",
+            "#9c6137",
             "#ef7d57",
             "#ffcd75",
             "#a7f070",
             "#38b764",
             "#257179",
-            "#29366f",
             "#3b5dc9",
-            "#41a6f6",
-            "#73eff7"
+            "#55cdfc",
+            "#ffaec9",
         });
 
         public static PartialCanvas CanvasChanges = new();
